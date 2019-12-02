04fcc2c8489c39f8b9d24038b4bf44d2ff04bd57
Laetitia Puissieux <puissieux@me.com>
2019-12-02 17:23:51 +0100

    first commit

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -1,10 +1,11 @@
 <?php
 // src/Controller/WildController.php
 namespace App\Controller;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-Class WildController extends AbstractController
+class WildController extends AbstractController
 {
     /**
      * @Route("/wild", name="wild_index")
@@ -12,10 +13,28 @@ Class WildController extends AbstractController
     public function index(): Response
     {
         return $this->render('wild/index.html.twig', [
-            'website' => 'Wild Séries',
+            'website'=> 'Wild Series'
         ]);
     }
+    /**
+     * @Route("/wild/show/{slug}",
+     *     requirements={"slug"="[a-zA-Z0-9\-]+"},
+     *     defaults={"slug"=null}),
+     *     name="wild_show",
+     */
+
+    public function show($slug) : Response
+    {
+        if (null === $slug) {
+            $newSlug = "Aucune serie selectionne, veuillez choisir une serie";
+        } else {
+            $newSlug = str_replace("-", ' ', $slug);
+            $newSlug = ucwords($newSlug);
+        }
+        return $this->render('wild/index.html.twig', ['slug'=>$newSlug]);
+    }
 }
+
 
 
 
